0ebcf957e25c9953a375e3afba47414a6f82c8f6
Firdavs-coder <shodiyev-f@mail.ru>
2021-12-05 14:16:51 +0500

    -

---
+++ helpers.py
@@ -0,0 +1,115 @@
+inf = ['Agriculture', 'Hosting', 'Alternative Power', 'Hotels', 'Animals or Pets', 'Icons',
+'Antique', 'Industry', 'App or Application', 'Interface', 'Architecture', 
+'Interior or Furniture', 'Art or Photography', 'Internet', 'Beauty', 'Jewellery', 
+'Blog', 'jQuery', 'Books', 'Law', 'Brewery', 'Lawyer or Legal', 'Business', 'Lifestyle',
+'Bootstrap', 'Magazine', 'Cafe or Restaurant', 'Marketing', 'Cars', 'Media', 'Celebration', 
+'Medical', 'Charity', 'Military', 'Christmas', 'Minimalistic', 
+'Clean', 'Multipurpose', 'Communications', 'Music', 'Computers', 'News', 'Control Panel', 
+'Night Club', 'Corporate', 'Online Shop', 'Dating', 'One Page', 'Design', 'Outdoors', 'Eco',
+'Personal', 'eCommerce', 'Politics', 'Education', 'Portal', 'Electronics', 'Portfolio',
+'Entertainment', 'Premium', 'Environmental', 'Real Estate', 'Events', 'Religion',
+'Exterior Design', 'Responsive', 'Family', 'School', 'Fashion', 'Science', 'Finance',
+'Security', 'Fitness', 'Services', 'Flowers', 'Society or Culture', 'Food or Drink',
+'Software', 'Forum', 'Sport', 'Gallery', 'Tools or Equipment', 'Games', 'Transport',
+'Gifts', 'Travel', 'Government', 'TV or Radio', 'Health or Healthcare', 'Webdesign',
+'Holiday', 'Wedding']
+
+text = """
+/Agriculture
+/Hosting
+/Alternative-Power
+/Hotels
+/Animals-or-Pets
+/Icons
+/Antique
+/Industry
+/App-or-Application
+/Interface
+/Architecture
+/Interior-or-Furniture
+/Art-or-Photography
+/Internet
+/Beauty
+/Jewellery
+/Blog
+/jQuery
+/Books
+/Law
+/Brewery
+/Lawyer-or-Legal
+/Business
+/Lifestyle
+/Bootstrap
+/Magazine
+/Cafe-or-Restaurant
+/Marketing
+/Cars
+/Media
+/Celebration
+/Medical
+/Charity
+/Military
+/Christmas
+/Minimalistic
+/Clean
+/Multipurpose
+/Communications
+/Music
+/Computers
+/News
+/Control-Panel
+/Night-Club
+/Corporate
+/Online-Shop
+/Dating
+/One-Page
+/Design
+/Outdoors
+/Eco
+/Personal
+/eCommerce
+/Politics
+/Education
+/Portal
+/Electronics
+/Portfolio
+/Entertainment
+/Premium
+/Environmental
+/Real-Estate
+/Events
+/Religion
+/Exterior-Design
+/Responsive
+/Family
+/School
+/Fashion
+/Science
+/Finance
+/Security
+/Fitness
+/Services
+/Flowers
+/Society-or-Culture
+/Food-or-Drink
+/Software
+/Forum
+/Sport
+/Gallery
+/Tools-or-Equipment
+/Games
+/Transport
+/Gifts
+/Travel
+/Government
+/TV-or-Radio
+/Health-or-Healthcare
+/Webdesign
+/Holiday
+/Wedding
+"""
+# for i in inf:
+#     o = i.replace(' ','-')
+#     text += f'/{o} \n'
+# print(text)
+
---
+++ template.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import telebot
+from telebot import types
+from helpers import *
+bot = telebot.TeleBot("5043926452:AAFLpliMElJViSDo9bOZ-tGluKjmBJ4CCg0", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+
+@bot.message_handler(commands=['menu'])
+def echo_all(message):
+    bot.send_message(message.chat.id, text)
+
+
+links = []
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    if message.text[0] == '/':
+        text = message.text[0:]
+        text = text.replace(' ','-')
+    else:
+        text = message.text
+        text = text.replace(' ','-')
+    sahifa = f"https://www.free-css.com/template-categories/{text}"
+    r = requests.get(sahifa)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    try:
+        get_three = soup.select('ul[class="clear"] li ')[:4]
+        media = []
+        links.clear()
+        for i in get_three:
+            links.append('https://www.free-css.com' + i.select('a')[0]['href'])
+            media.append(types.InputMediaPhoto('https://www.free-css.com' +  i.select('img')[0]['src']))
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        itembtna = types.InlineKeyboardButton('1', callback_data='0')
+        itembtna1 = types.InlineKeyboardButton('2', callback_data='1')
+        itembtna2 = types.InlineKeyboardButton('3', callback_data='2')
+        itembtna3 = types.InlineKeyboardButton('4', callback_data='3')
+        markup.add(itembtna,itembtna1,itembtna2,itembtna3)
+        bot.send_media_group(message.chat.id, media)
+        bot.send_message(message.chat.id, 'Choose one', reply_markup=markup)
+    except:
+        bot.send_message(message.chat.id, 'Not found')
+    
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    sahifa2 = links[int(call.data)]
+    r2 = requests.get(sahifa2)
+    soup = BeautifulSoup(r2.text, 'html.parser')
+    zip = 'https://www.free-css.com' +  soup.select('ul[class="clear"] li[class="dld"] a')[0]["href"]
+    bot.send_document(call.from_user.id, zip)
+
+bot.infinity_polling()
